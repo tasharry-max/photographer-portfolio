27d7fd81ac6c5b184fb9fbe22c153efadb7f3f34
KYIV\nrud <nataly.rudnitskaya@gmail.com>
2020-12-23 17:44:25 +0200

    services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 //Page Components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
     return (
         <div>
             <AboutSection />
+            <ServicesSection />
         </div>
     )
 }
